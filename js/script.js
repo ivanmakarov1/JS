cc91cc6edb6398415ea2df5f5a50bdd961fad1ca
ivanmakarov1 <ivanmakarov1@yandex.ru>
2021-10-03 11:48:43 +0300

    save2

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//let InputIn = document.querySelector('.input-in'); // input монипуляция с инпут и занисение в переменную.
+//let button = document.querySelector('button'); // button монипуляция с кнопкой.
+//let div = document.querySelector('.out');
+//button.onclick = function() {
+    // сработает после нажатия на кнопку(событие).
+    //console.log(InputIn.value); // то что введено в инпут.
+    //let b = +InputIn.value; // + перевести в число.
+    //console.log(b + 5);
+    //div.innerHTML = b; //выводит в див что написано в инпут.
+    //InputIn.value = ''; // стирает поле инпут.
+
+//}
+
+//task1
